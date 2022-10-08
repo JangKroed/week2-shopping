3479be8a286ce5912a643c38da34199bb802a1ba
JangKroed <didlsdydgh@gmail.com>
2022-10-08 21:09:40 +0900

    mysql 명령어

--- SHOW TABLES.sql
+++ SHOW TABLES.sql
@@ -1,9 +1,7 @@
 -- 테이블 확인
 -- SHOW TABLES
-
 -- 데이터베이스 생성
 -- CREATE DATABASE NodeJS;
-
 -- 테이블 생성
 -- CREATE TABLE IF NOT EXISTS courses (
 --     id bigint(5) NOT NULL AUTO_INCREMENT, 
@@ -11,26 +9,21 @@
 --     tutor varchar(255) NOT NULL,
 --     PRIMARY KEY (id)
 -- );
-
 -- 데이터 삽입
 -- INSERT INTO courses (title, tutor) VALUES
 --     ('Node.js 숙련반', '이용우'), ('웹개발 종합반', '이범규');
-
 -- 데이터 조회
-SELECT * FROM courses;
-
+-- SELECT * FROM courses;
 -- 음식 주문앱 DB설계 예제
 -- CREATE TABLE User(
 --     userId int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 --     name varchar(255) NOT NULL UNIQUE
 -- );
-
 -- CREATE TABLE Food(
 --     foodId int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 --     name varchar(255),
 --     price int(11)
 -- );
-
 -- CREATE TABLE Order(
 --     orderId int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 --     userId int(11) NOT NULL, 
@@ -39,19 +32,23 @@ SELECT * FROM courses;
 --     FOREIGN KEY (foodId) REFERENCES Food(foodId) ON DELETE CASCADE,
 --     FOREIGN KEY (userId) REFERENCES User(userId) ON DELETE CASCADE
 -- );
-
 -- SQL 연습
 -- 테이블 생성
-CREATE TABLE IF NOT EXISTS users (
-    id bigint(5) NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    age bigint(5) NOT NULL,
-    PRIMARY KEY (id)
-);
-
+-- CREATE TABLE IF NOT EXISTS users (
+--     id bigint(5) NOT NULL AUTO_INCREMENT,
+--     name varchar(255) NOT NULL,
+--     age bigint(5) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 -- 데이터 삽입
-INSERT INTO users (name, age) VALUES
-    ('이용우', 28);
-
+INSERT INTO Goods (name, thumbnailUrl, category, price)
+VALUES (
+        '상품',
+        "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
+        "drink",
+        0.11
+    );
 -- 데이터 조회
-SELECT * FROM users;
+-- SELECT * FROM Goods;
+-- 테이블 삭제
+-- DROP TABLE Goods;
